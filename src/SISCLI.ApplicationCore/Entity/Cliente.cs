793cd904c81b60d930507e4abc95ad341d8a117b
migueldossantos011@gmail.com <migueldossantos011@gmail.com>
2020-10-22 20:50:09 -0300

    Adicionar arquivos de projeto.

---
+++ src/SISCLI.ApplicationCore/Entity/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SISCLI.ApplicationCore.Entity
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+
+        }
+
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+    }
+}
